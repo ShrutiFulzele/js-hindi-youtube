1f610b3336198e7ebfacd5cd0d10a67599c2fb86
Shruti Fulzele <ShrutiFulzele7499@gmail.com>
2025-02-16 17:32:30 +0000

    04_file

--- 01_basic/03_file.js
+++ 01_basic/03_file.js
@@ -26,5 +26,30 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 56
 
-let StringNumber = String(someNumber)
-console.log(typeof StringNumber);
+// let StringNumber = String(someNumber)
+// console.log(typeof StringNumber);
+
+
+// **************Oprations***********
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2%2);
+
+
+let str1 = "shruti"
+let str2 = "_shruit"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+ "2");
+console.log(1+ 2+"2");
+
