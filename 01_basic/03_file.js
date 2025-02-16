6637602bc8f62894a6a3351489934d58e4cdc6d7
Shruti Fulzele <ShrutiFulzele7499@gmail.com>
2025-02-16 17:00:31 +0000

    conversion of deta type

---
+++ 01_basic/03_file.js
@@ -0,0 +1,30 @@
+let score = true
+
+
+// console.log(typeof Score);
+// console.log(typeof(score));
+
+let valuInNumber = Number(score)
+// console.log(typeof valuInNumber);
+// console.log( valuInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN (not an numberf)
+// true => 1; false => 0
+
+let isLoggedIn = "564"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// ""  =>  false
+// "shruti" => true
+//  "562" => true
+
+let someNumber = 56
+
+let StringNumber = String(someNumber)
+console.log(typeof StringNumber);
---
+++ 01_basic/demo.js
@@ -0,0 +1,15 @@
+const accountID = 123456
+let accountEmail = "jsdybeac@gmail.com"
+var accountPassword = "13456"
+accountCity = "Nagpur"
+let accountState;
+
+console.log(accountEmail)
+
+accountEmail = "Shruti@gmail.com"
+accountPassword ="123654"
+accountCity = "Pune"
+
+console.log(accountID);
+
+console.table([accountEmail,accountID,accountPassword,accountCity,accountState]);
